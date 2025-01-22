ad20f81f69aa8144d6030b126c0d01607f280f10
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-22 15:48:04 +0900

    Implement page transition on the global navigation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,15 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
 
         if user&.authenticate(params[:session][:password])
+            log_in(user)
             redirect_to tasks_path
         else
             render :new
         end
+    end
+
+    def destroy
+        session.delete(:user_id)
+        redirect_to new_session_path
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,13 @@
 module SessionsHelper
     def current_user
-        User.first
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
     
     def logged_in?
-        true
+        current_user.present?
+    end
+
+    def log_in(user)
+        session[:user_id] = user.id
     end
 end

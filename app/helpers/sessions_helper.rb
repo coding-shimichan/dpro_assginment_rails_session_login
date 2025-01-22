4d6a69cfb8dd0962482b116f79c09a8f553fa86e
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-22 15:34:23 +0900

    Add global navigation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+    def current_user
+        User.first
+    end
+    
+    def logged_in?
+        true
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users
   root 'tasks#index'
   resources :tasks
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end

7a3a6c879bced18ea2ec7d6b2d69fb53f627b1e7
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-22 14:51:15 +0900

    Add create to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+
+        if user&.authenticate(params[:session][:password])
+            render json: { result: "Successfully logged in!", user: user}
+        else
+            render json: { result: "Login failed!"}
+        end
+    end
 end

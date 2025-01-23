7c103747544fa3467e8f1b6c5d4429b3dc9defb9
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 18:17:57 +0900

    Remove the route for the users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create, :destroy, :show, :edit]
   root 'tasks#index'
   resources :tasks
   resources :sessions, only: [:new, :create, :destroy]

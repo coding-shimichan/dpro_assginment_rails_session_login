9568c001a82ca776fc2ce9101d1d46f708032557
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 19:04:18 +0900

    Revert "Check presence of password_confirmation"
    This reverts commit 79772364d5a37555536c7ced6ff5cfa428e47234.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, too_short: "は%{count}文字以上で入力してください" }
-    validates :password_confirmation, presence: true
 end

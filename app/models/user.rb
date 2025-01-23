572209b22d24f8da84c3f9c26552485f5dd33b2c
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 14:22:50 +0900

    Add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, confirmation: true, presence: true, length: { minimum: 6, too_short: "は%{count}文字以上で入力してください" }
 end

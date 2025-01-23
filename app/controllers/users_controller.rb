073c45621ff7aca5bae1afd6fb8084a1a514cf90
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 13:56:37 +0900

    Transition after a sign-up attempt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,15 +23,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        log_in(@user)
-        format.html { redirect_to tasks_path }
-        format.json { redirect_to tasks_path }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      log_in(@user)
+      redirect_to tasks_path
+    else
+      render :new
     end
   end
 

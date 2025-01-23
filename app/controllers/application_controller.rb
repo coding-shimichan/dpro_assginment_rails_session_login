b71da3340818c123452b4bb1caec44256e2cbb2a
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 18:09:08 +0900

    Show flash alert when accessing pages without logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
     def require_login
         if !(logged_in?)
-            flash[:alert] = "Login required"
+            flash[:alert] = t("common.login_required")
             redirect_to new_session_path
         end
     end

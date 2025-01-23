79772364d5a37555536c7ced6ff5cfa428e47234
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 18:40:10 +0900

    Check presence of password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, too_short: "は%{count}文字以上で入力してください" }
+    validates :password_confirmation, presence: true
 end

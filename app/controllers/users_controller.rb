a61dfb427de4df9509c7550a3ccad1475da0307a
tomoe.shimizu <shimizu.tomoe.ts@gmail.com>
2025-01-23 17:24:49 +0900

    Redirect to login page when user is deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,10 +50,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to new_session_path
   end
 
   private
